4d82466a931df37acd380753320d12550e22a6fa
anujshukla-1987 <53939841+anujshukla-1987@users.noreply.github.com>
2019-09-04 15:57:14 +0200

    Update InventoryApplication.java

--- src/main/java/com/springboot/inventory/InventoryApplication.java
+++ src/main/java/com/springboot/inventory/InventoryApplication.java
@@ -21,10 +21,45 @@ public class InventoryApplication {
      
     @Autowired
     Job job;
+    
+    /* Uncomment for initial setup on localDB
+     
+     private DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
+    
+	@Autowired
+	ProductRepository productRepository;
+    
+    @PostConstruct
+	public void init() {
+		
+		createProductCatalogTable();
+		Product product1 = new Product("Item1", "Location1", false, new BigDecimal("123.12"));
+		productRepository.save(product1);
+		
+		Product product2 = new Product("Item2", "Location2", false, new BigDecimal("124132.12"));
+		productRepository.save(product2);		
+	}
+
+	public void createProductCatalogTable(){
+        try {
+            dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+
+            CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(Product.class);
+
+            tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+            amazonDynamoDB.createTable(tableRequest);
+        } catch (ResourceInUseException e) {
+            
+        }
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
-	}
+	} 
 	
 	@Scheduled(cron = "0 */1 * * * ?")
     public void perform() throws Exception
